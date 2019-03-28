a22f06f736dadc79f0586286589c16f7adee0397
Navin <navin.lubhaya@proagrica.com>
2019-03-28 12:28:45 +0000

    add tests per unit of time

--- src/test/java/com/adaptris/samples/DateCompareServiceTest.java
+++ src/test/java/com/adaptris/samples/DateCompareServiceTest.java
@@ -19,16 +19,33 @@ public class DateCompareServiceTest extends ServiceCase {
   public void testService() throws Exception {
     AdaptrisMessage adaptrisMessage = AdaptrisMessageFactory.getDefaultInstance().newMessage();
     adaptrisMessage.addMetadata("key1", "2019-01-08");
-    adaptrisMessage.addMetadata("key2", "2019-01-04");
+    adaptrisMessage.addMetadata("key2", "2019-01-07");
     DateCompareService dcs = new DateCompareService();
     dcs.setStartDateMetadataKey("key1");
     dcs.setEndDateMetadataKey("key2");
     dcs.setDateFormat("yyyy-MM-dd");
     dcs.setOutputMetadataKey("daysDifference");
+
     dcs.setUnitOfTime(TimeUnit.DAYS);
     dcs.doService(adaptrisMessage);
-    String difference = dcs.getUnitDifferenceValue();
-    assertEquals("4", difference);            //expected (String) vs actual (String)
+    String difference_days = dcs.getUnitDifferenceValue();
+    assertEquals("1", difference_days);            //expected (String) vs actual (String)
+
+    dcs.setUnitOfTime(TimeUnit.HOURS);
+    dcs.doService(adaptrisMessage);
+    String difference_hours = dcs.getUnitDifferenceValue();
+    assertEquals("24", difference_hours);            //expected (String) vs actual (String)
+
+    dcs.setUnitOfTime(TimeUnit.MINUTES);
+    dcs.doService(adaptrisMessage);
+    String difference_minutes = dcs.getUnitDifferenceValue();
+    assertEquals("1440", difference_minutes);            //expected (String) vs actual (String)
+
+    dcs.setUnitOfTime(TimeUnit.SECONDS);
+    dcs.doService(adaptrisMessage);
+    String difference_seconds = dcs.getUnitDifferenceValue();
+    assertEquals("86400", difference_seconds);            //expected (String) vs actual (String)
+
   }
 
   @Override
